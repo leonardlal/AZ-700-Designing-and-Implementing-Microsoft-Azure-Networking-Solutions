0e4b55a73e4fdfd80d383ff11699969e2be5feb2
Leo <64842507+leonardlal@users.noreply.github.com>
2023-10-05 12:25:36 +0800

    Update Install IIS.ps1
    Add the VM details in the html file

--- Scripts/Install IIS.ps1
+++ Scripts/Install IIS.ps1
@@ -1,1 +1,25 @@
-Install-WindowsFeature -name Web-Server -IncludeManagementTools
+# Install IIS and required components
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+
+# Get the VM's hostname, private IP address, and Azure region
+$hostname = $env:COMPUTERNAME
+$ipAddress = (Get-NetIPAddress | Where-Object {$_.AddressFamily -eq "IPv4" -and $_.InterfaceAlias -eq "Ethernet"}).IPAddress
+$region = (Invoke-RestMethod -Uri "http://169.254.169.254/metadata/instance/compute/location?api-version=2020-06-01&format=text").ToLower()
+
+# Create an HTML file with the VM information
+$htmlContent = @"
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Azure VM Info</title>
+</head>
+<body>
+    <h1>Hostname: $hostname</h1>
+    <h1>IP Address: $ipAddress</h1>
+    <h1>Region: $region</h1>
+</body>
+</html>
+"@
+
+# Save the HTML content to the default IIS website folder
+$htmlContent | Set-Content -Path "C:\inetpub\wwwroot\default.html" -Force
