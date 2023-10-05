4899e90c4f991fef93f6a6ce7683d93a2e3c3ecc
Leo <64842507+leonardlal@users.noreply.github.com>
2023-10-05 14:19:35 +0800

    Update Install IIS.ps1
    update metadata api version

--- Scripts/Install IIS.ps1
+++ Scripts/Install IIS.ps1
@@ -1,10 +1,11 @@
+<powershell>
 # Install IIS and required components
 Install-WindowsFeature -Name Web-Server -IncludeManagementTools -LogPath "$env:TEMP\init-webservervm_webserver_install_log_$logLabel.txt"
 
 # Get the VM's hostname, private IP address, and Azure region
 $hostname = $env:COMPUTERNAME
 $ipAddress = (Get-NetIPAddress | Where-Object {$_.AddressFamily -eq "IPv4" -and $_.InterfaceAlias -eq "Ethernet"}).IPAddress
-$region = (Invoke-RestMethod -Uri "http://169.254.169.254/metadata/instance/compute/location?api-version=2020-06-01&format=text").ToLower()
+$region = (Invoke-RestMethod -Uri "http://169.254.169.254/metadata/instance/compute/location?api-version=$apiVersion&format=text").ToLower()
 
 # Create an HTML file with the VM information
 $htmlContent = @"
@@ -23,3 +24,5 @@ $htmlContent = @"
 
 # Save the HTML content to the default IIS website folder
 $htmlContent | Set-Content -Path "C:\inetpub\wwwroot\default.html" -Force
+
+</powershell>
